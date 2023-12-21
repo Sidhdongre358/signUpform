326c7797b2065d5363f136432f3395fdb0e10d0b
Sidhdongre358 <sidhdongre358@gmail.com>
2023-12-21 09:38:49 +0530

    Initial commit

---
+++ jscript.js
@@ -0,0 +1,7 @@
+const container=document.querySelector(".container");
+
+const signUpBtn = document.querySelector(".blue-bg button");
+
+signUpBtn.addEventListener("click",()=>{
+container.classList.toggle('change');
+});
